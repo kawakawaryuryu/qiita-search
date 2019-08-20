e0aba70da24964fb02cee3f8cc3593e3454ebb25
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2019-08-21 00:35:56 +0900

    add typescript

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,7 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    // extend(config, ctx) {}
+  },
+  buildModules: ['@nuxt/typescript-build']
 }
